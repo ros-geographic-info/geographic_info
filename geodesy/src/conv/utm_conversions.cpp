fd3090e67e6c801fb2a4a8dee3da91c747192966
Jack O'Quin <jack.oquin@gmail.com>
2011-07-04 11:56:37 -0500

    implement UTMPoint constructor from GeoPoint using convert()

--- geodesy/src/conv/utm_conversions.cpp
+++ geodesy/src/conv/utm_conversions.cpp
@@ -260,86 +260,10 @@ void convert(const geographic_msgs::GeoPoint &from, UTMPoint &to)
     }
 }
 
-/** Create UTM point from WGS 84 geodetic point.
- *
- *  Equations from USGS Bulletin 1532
- *
- *  @todo implement using convert()
- */
+/** Create UTM point from WGS 84 geodetic point. */
 UTMPoint::UTMPoint(const geographic_msgs::GeoPoint &pt)
 {
-  double Lat = pt.latitude;
-  double Long = pt.longitude;
-
-  double a = WGS84_A;
-  double eccSquared = UTM_E2;
-  double k0 = UTM_K0;
-
-  double LongOrigin;
-  double eccPrimeSquared;
-  double N, T, C, A, M;
-	
-  //Make sure the longitude is between -180.00 .. 179.9
-  double LongTemp = (Long+180)-int((Long+180)/360)*360-180;
-  double LatRad = angles::from_degrees(Lat);
-  double LongRad = angles::from_degrees(LongTemp);
-  double LongOriginRad;
-
-  this->altitude = pt.altitude;
-  this->zone = int((LongTemp + 180)/6) + 1;
-  
-  if( Lat >= 56.0 && Lat < 64.0 && LongTemp >= 3.0 && LongTemp < 12.0 )
-    this->zone = 32;
-
-  // Special zones for Svalbard
-  if( Lat >= 72.0 && Lat < 84.0 ) 
-    {
-      if(      LongTemp >= 0.0  && LongTemp <  9.0 ) this->zone = 31;
-      else if( LongTemp >= 9.0  && LongTemp < 21.0 ) this->zone = 33;
-      else if( LongTemp >= 21.0 && LongTemp < 33.0 ) this->zone = 35;
-      else if( LongTemp >= 33.0 && LongTemp < 42.0 ) this->zone = 37;
-    }
-  // +3 puts origin in middle of zone
-  LongOrigin = (this->zone - 1)*6 - 180 + 3; 
-  LongOriginRad = angles::from_degrees(LongOrigin);
-
-  // compute the UTM band from the latitude
-  this->band = UTMLetterDesignator(Lat);
-#if 0
-  if (this->band == ' ')
-    throw std::range_error;
-#endif
-
-  eccPrimeSquared = (eccSquared)/(1-eccSquared);
-
-  N = a/sqrt(1-eccSquared*sin(LatRad)*sin(LatRad));
-  T = tan(LatRad)*tan(LatRad);
-  C = eccPrimeSquared*cos(LatRad)*cos(LatRad);
-  A = cos(LatRad)*(LongRad-LongOriginRad);
-
-  M = a*((1 - eccSquared/4 - 3*eccSquared*eccSquared/64
-          - 5*eccSquared*eccSquared*eccSquared/256) * LatRad 
-         - (3*eccSquared/8 + 3*eccSquared*eccSquared/32
-            + 45*eccSquared*eccSquared*eccSquared/1024)*sin(2*LatRad)
-         + (15*eccSquared*eccSquared/256
-            + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad) 
-         - (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
-	
-  this->easting = (double)
-    (k0*N*(A+(1-T+C)*A*A*A/6
-           + (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
-     + 500000.0);
-
-  this->northing = (double)
-    (k0*(M+N*tan(LatRad)
-         *(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
-           + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
-
-  if(Lat < 0)
-    {
-      //10000000 meter offset for southern hemisphere
-      this->northing += 10000000.0;
-    }
+  convert(pt, *this);
 }
 
 /** Transform UTM point to WGS 84 geodetic point.
