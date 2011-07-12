2857eaccd704758141dfa5d9189ad5e9928c1caa
Jack O'Quin <jack.oquin@gmail.com>
2011-07-11 21:06:56 -0500

    fill in artificial covariance data values

--- navsat_odometry/tests/gendata.py
+++ navsat_odometry/tests/gendata.py
@@ -22,16 +22,49 @@ def test(hz):
     gps_pub = rospy.Publisher('gps', NavSatFix)
     imu_pub = rospy.Publisher('imu', Imu)
 
-    # set position at University of Texas, Austin, Pickle Research Campus
     fix = NavSatFix()
+    fix.header.frame_id = "/odom"
+
+    # set position at University of Texas, Austin, Pickle Research Campus
     fix.latitude = 30.385315
     fix.longitude = -97.728524
     fix.altitude = 209.0
-    fix.header.frame_id = "/odom"
+
+    # set covariance matrix to known, artificial values
+    fix.position_covariance_type = NavSatFix.COVARIANCE_TYPE_KNOWN
+    fix.position_covariance[0] = 00.0
+    fix.position_covariance[1] = 01.0
+    fix.position_covariance[2] = 02.0
+    fix.position_covariance[3] = 10.0
+    fix.position_covariance[4] = 11.0
+    fix.position_covariance[5] = 12.0
+    fix.position_covariance[6] = 20.0
+    fix.position_covariance[7] = 21.0
+    fix.position_covariance[8] = 22.0
 
     # set IMU not moving
     imu = Imu()
     imu.header.frame_id = "/base_link"
+
+    # set IMU covariance matrices to known, artificial values
+    imu.orientation_covariance[0] = 33.0
+    imu.orientation_covariance[1] = 34.0
+    imu.orientation_covariance[2] = 35.0
+    imu.orientation_covariance[3] = 43.0
+    imu.orientation_covariance[4] = 44.0
+    imu.orientation_covariance[5] = 45.0
+    imu.orientation_covariance[6] = 53.0
+    imu.orientation_covariance[7] = 54.0
+    imu.orientation_covariance[8] = 55.0
+    imu.angular_velocity_covariance[0] = 33.0
+    imu.angular_velocity_covariance[1] = 34.0
+    imu.angular_velocity_covariance[2] = 35.0
+    imu.angular_velocity_covariance[3] = 43.0
+    imu.angular_velocity_covariance[4] = 44.0
+    imu.angular_velocity_covariance[5] = 45.0
+    imu.angular_velocity_covariance[6] = 53.0
+    imu.angular_velocity_covariance[7] = 54.0
+    imu.angular_velocity_covariance[8] = 55.0
 
     while not rospy.is_shutdown():
 
