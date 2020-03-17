8d3fd4e91a6293d2912cc2b1151c388df3a2dde2
stevemacenski <stevenmacenski@gmail.com>
2020-03-17 10:57:03 -0700

    convert python3 for noetic

--- geodesy/setup.py
+++ geodesy/setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+
+try:
+    from distutils.core import setup
+except ImportError:
+    try:
+      from setuptools import setup
+    except ImportError:
+      assert False, 'Must have distutils or setuptools installed!'
 
 d = generate_distutils_setup(
     packages=['geodesy'],
