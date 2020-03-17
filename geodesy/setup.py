f3521d64abfafaeccb10f607f27961ea1ee75107
stevemacenski <stevenmacenski@gmail.com>
2020-03-17 11:18:13 -0700

    adding PR requests

--- geodesy/setup.py
+++ geodesy/setup.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 
 from catkin_pkg.python_setup import generate_distutils_setup
-
-try:
-    from distutils.core import setup
-except ImportError:
-    try:
-      from setuptools import setup
-    except ImportError:
-      assert False, 'Must have distutils or setuptools installed!'
+from setuptools import setup
 
 d = generate_distutils_setup(
     packages=['geodesy'],
